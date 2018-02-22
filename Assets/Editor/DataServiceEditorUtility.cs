20e94176341373268d67f0fa6a879a08d9740192
Rodrigo Jurado <jotasimp32@gmail.com>
2018-02-22 18:55:08 +0100

    Añadidas nuevas opciones al menú de utilidades para añadir datos de setas a los contenedores de datos. Añadida una lista de contenedores para gestionar mejor los contenedores creados que se van a utilizar.

--- Assets/Editor/DataServiceEditorUtility.cs
+++ Assets/Editor/DataServiceEditorUtility.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using Database.DataStructures;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -7,14 +9,70 @@ using UnityEngine;
 public class DataServiceEditorUtility : Editor
 {
     string text = "";
+    MushroomList list;
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
         DataService dataService = (DataService)target;
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("DB Utilities", EditorStyles.boldLabel);
         text = EditorGUILayout.TextField(label:"Mushroom Name", text:text);
-        if (GUILayout.Button("Read Mushroom"))
+        if (GUILayout.Button("Read Mushroom from DB"))
         {
             dataService.GetMushroomByName(text);
         }
+        if(!string.IsNullOrEmpty(text) && GUILayout.Button("Create Mushroom ScriptableObject"))
+        {
+            MushroomScriptableObject scriptableObj = CreateMushroomScriptableObject(dataService);
+            if (scriptableObj != null)
+            {
+                SaveScriptableObject(scriptableObj.Name, scriptableObj);
+            }
+        }
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Mushroom Data assets generator", EditorStyles.boldLabel);
+        if (GUILayout.Button("Create Mushrooms List"))
+        {
+            MushroomScriptableObject scriptableObj = CreateMushroomScriptableObject(dataService);
+            if (scriptableObj != null)
+            {
+                MushroomList list = CreateMushroomList();
+                list.mushroomList.Add(scriptableObj);
+                SaveScriptableObject(scriptableObj.Name, scriptableObj);
+            }
+        }
+    }
+
+    private MushroomList CreateMushroomList()
+    {
+        if (list == null)
+        {
+            list = CreateInstance<MushroomList>();
+            AssetDatabase.CreateAsset(list, "Assets/Data/Mushrooms/MushroomList.asset");
+            list.mushroomList = new List<MushroomScriptableObject>();
+        }
+        return list;
+    }
+
+    private MushroomScriptableObject CreateMushroomScriptableObject(DataService dataService)
+    {
+        Mushroom mushroom = dataService.GetMushroomByName(text);
+        if (mushroom != null)
+        {
+            MushroomScriptableObject asset = CreateInstance<MushroomScriptableObject>();
+            asset.Name = mushroom.Name;
+            asset.Description = mushroom.Description;
+            asset.EdibleCondition = mushroom.EdibleCondition;
+            asset.Species = mushroom.Species;
+            AssetDatabase.CreateAsset(asset, "Assets/Data/Mushrooms/" + mushroom.Name + ".asset");
+            return asset;
+        }
+
+        return null;
+    }
+
+    private void SaveScriptableObject(string objectName, ScriptableObject asset)
+    {
+        AssetDatabase.SaveAssets();
     }
 }
---
+++ Assets/Scripts/ScriptableObjects/MushroomList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MushroomList", menuName = "UBUSetas/MushroomList", order = 2)]
+public class MushroomList : ScriptableObject
+{
+    public List<MushroomScriptableObject> mushroomList;
+}
