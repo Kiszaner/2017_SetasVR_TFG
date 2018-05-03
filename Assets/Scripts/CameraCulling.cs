25b5182a89c2637e80e62d21ba1dfc2ca8e16761
Rodrigo Jurado <jotasimp32@gmail.com>
2018-05-03 22:56:36 +0200

    Actualizada la base de datos con nuevas setas. Actualizados los modelos de algunas de las setas existentes. Actualizados algunos prefabs para reflejar los nuevos cambios. Añadido script para ocultar los elementos del escenario que están lejos y ganar algo de rendimiento gráfico.

---
+++ Assets/Scripts/CameraCulling.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCulling : MonoBehaviour
+{
+    public float distance = 15;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Camera camera = GetComponent<Camera>();
+        float[] distances = new float[32];
+        distances[9] = distance;
+        camera.layerCullDistances = distances;
+    }
+}
