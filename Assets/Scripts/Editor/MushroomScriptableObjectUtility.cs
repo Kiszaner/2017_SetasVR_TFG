636091a3f3d0053b7f12e9ceceeaca4599f2b4e9
Rodrigo Jurado <jotasimp32@gmail.com>
2018-06-05 13:48:39 +0200

    Añadido el plugin Extended Colliders 3D para poder añadir mesh colliders con formas simples. Modificado el collider de las setas para hacerlo más simple y mejorar el rendimiento de la aplicación, así como intentar solventar los errores internos que se generaban en tiempo de ejecución en el móvil. Actualizado el nivel 1-2 para refrescar la colocación de las setas. Limpiado el código de la herramienta de edición de los contenedores de datos.

--- Assets/Scripts/Editor/MushroomScriptableObjectUtility.cs
+++ Assets/Scripts/Editor/MushroomScriptableObjectUtility.cs
@@ -26,36 +26,11 @@ public class MushroomScriptableObjectUtility : Editor
             foreach (string sFilePath in aFilePaths)
             {
                 string sAssetPath = sFilePath.Substring(sDataPath.Length - 6);
-                //Debug.Log(sAssetPath);
-
                 Object objAsset = AssetDatabase.LoadAssetAtPath(sAssetPath, typeof(Object));
-
                 textures.Add((Texture2D)objAsset);
-
-                //Debug.Log(objAsset.GetType().Name);
             }
             RandomPickPhotos(mushScrObj, textures);
         }
-        //if (GUILayout.Button("Asset Path"))
-        //{
-        //    string sDataPath = Application.dataPath;
-        //    string capitalizedMushName = AuxiliarFunctions.FirstUpper(mushScrObj.Name);
-        //    string sFolderPath = sDataPath.Substring(0, sDataPath.Length - 6) + "Assets/Art/2D/Mushrooms/" +  capitalizedMushName;
-        //    Debug.Log("A: " + sFolderPath);
-        //    string[] aFilePaths = Directory.GetFiles(sFolderPath, "*.jpg");
-        //    textures = new List<Texture>();
-        //    foreach (string sFilePath in aFilePaths)
-        //    {
-        //        string sAssetPath = sFilePath.Substring(sDataPath.Length - 6);
-        //        Debug.Log(sAssetPath);
-
-        //        Object objAsset = AssetDatabase.LoadAssetAtPath(sAssetPath, typeof(Object));
-
-        //        textures.Add((Texture2D)objAsset);
-
-        //        Debug.Log(objAsset.GetType().Name);
-        //    }
-        //}
     }
 
     public void RandomPickPhotos(MushroomScriptableObject mushScrObj, List<Texture> textures)
